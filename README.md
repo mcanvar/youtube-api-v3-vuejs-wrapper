bbcfed5c618861970a2fe3271ec62d09207a2f3c
Mevlut Canvar <mevlutcanvar@gmail.com>
2018-09-17 01:01:56 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
-# vue-youtube-api-v3-wrapper
+# Youtube API V3 Demo Vuejs Wrapper
+
+This is just a simple implementation of the Google 
+Youtube Data API v3 [subscriptions list](https://developers.google.com/youtube/v3/docs/subscriptions/list#request) javascript sample.
+
+It is wrapped by Vuejs app and styled by Bulma 
+Framework.
+
+
 
 ## Project setup
+#### Step One: Create an Google API App and Credentials 
+ 1.  Use [this wizard](https://console.developers.google.com/start/api?id=youtube) to create or select a project in the Google Developers Console and automatically turn on the API. Click **Continue**, then **Go to credentials**.
+2.  On the **Add credentials to your project** page, click the **Cancel** button.
+3.  At the top of the page, select the **OAuth consent screen** tab. Select an **Email address**, enter a **Product name** if not already set, and click the **Save** button.
+4.  Select the **Credentials** tab, click the **Create credentials** button and select **OAuth client ID**.
+5.  Select the application type **Web application**.
+6.  In the **Authorized JavaScript origins** field, enter the URL `http://localhost:8080`. Same for **Authorized redirect URIs** field.
+7.  Click the **Create** button.
+8.  Click **OK** to dismiss the resulting dialog.
+9.  Click **Download** button and download JSON file.
+10. Save it as **client_secret.json** on your project's root directory.
+
+#### Step Two: Build your project.
 ```
+git clone https://github.com/Crx4/youtube-api-v3-vuejs-wrapper.git
+cd youtube-api-v3-vuejs-wrapper
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,214 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <nav class="panel">
+            <div class="panel-heading">
+                <div class="columns">
+                    <div class="column">
+                        <h3>My Channel Subscriptions</h3>
+                    </div>
+                    <div class="column"></div>
+                    <div class="column"></div>
+                    <div class="column"></div>
+                    <div class="column">
+                        <nav class="pagination" role="navigation" aria-label="pagination">
+                            <a class="pagination-previous"
+                               @click="defineRequest(prevPageToken)"
+                               v-show="prevPageToken">Previous</a>
+                            <a class="pagination-next"
+                               @click="defineRequest(nextPageToken)"
+                               v-show="nextPageToken">Next page</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div v-if="hasErrors" class="panel-block has-text-danger">
+                <p>Please authorize first.</p>
+            </div>
+            <article v-else class="panel-block" v-for="(item, index) in items" :key="index">
+                <figure class="media-left">
+                    <p class="image is-64x64">
+                        <img :src="item.snippet.thumbnails.default.url">
+                    </p>
+                </figure>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>{{ item.snippet.title }}</strong>
+                            <br>
+                            {{ limitDescText(item.snippet.description) }}
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <div v-if="hasErrors" class="panel-block">
+                <button class="button is-link is-outlined is-fullwidth" @click="handleAuthClick()">
+                    Authorize
+                </button>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import clientJSON from '../client_secret.json'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                GoogleAuth: {},
+                credentials: clientJSON,
+                items: [],
+                hasErrors: false,
+                nextPageToken: null,
+                prevPageToken: null
+            }
+        },
+        methods: {
+            injectGapi() {
+                let self = this;
+                const script = document.createElement("script");
+                script.src = "https://apis.google.com/js/client.js";
+
+                script.onload = () => {
+                    window.gapi.load('client:auth2', () => {
+                        window.gapi.client.init({
+                            'clientId': self.credentials.web.client_id,
+                            'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+                            'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
+                        }).then(function () {
+                            self.GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+                            self.GoogleAuth.isSignedIn.listen(self.updateSigninStatus());
+
+                            // self.setSigninStatus();
+                        });
+                    });
+                };
+
+                document.body.appendChild(script);
+            },
+
+            setSigninStatus() {
+                let user = this.GoogleAuth.currentUser.get();
+                let isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
+                if (isAuthorized) {
+                    this.defineRequest();
+                }
+            },
+
+            updateSigninStatus() {
+                this.setSigninStatus();
+            },
+
+            defineRequest(pageToken = '') {
+                this.buildApiRequest('GET',
+                    '/youtube/v3/subscriptions',
+                    {
+                        'mine': 'true',
+                        'pageToken': this.pageToken(pageToken),
+                        'part': 'snippet,contentDetails'
+                    });
+            },
+
+            buildApiRequest(requestMethod, path, params, properties) {
+                params = this.removeEmptyParams(params);
+                let request;
+                if (properties) {
+                    let resource = this.createResource(properties);
+                    request = window.gapi.client.request({
+                        'body': resource,
+                        'method': requestMethod,
+                        'path': path,
+                        'params': params
+                    });
+                } else {
+                    request = window.gapi.client.request({
+                        'method': requestMethod,
+                        'path': path,
+                        'params': params
+                    });
+                }
+                this.executeRequest(request);
+            },
+
+            createResource(properties) {
+                let resource = {};
+                let normalizedProps = properties;
+                for (let p in properties) {
+                    let value = properties[p];
+                    if (p && p.substr(-2, 2) == '[]') {
+                        let adjustedName = p.replace('[]', '');
+                        if (value) {
+                            normalizedProps[adjustedName] = value.split(',');
+                        }
+                        delete normalizedProps[p];
+                    }
+                }
+                for (let r in normalizedProps) {
+                    // Leave properties that don't have values out of inserted resource.
+                    if (normalizedProps.hasOwnProperty(r) && normalizedProps[r]) {
+                        let propArray = r.split('.');
+                        let ref = resource;
+                        for (let pa = 0; pa < propArray.length; pa++) {
+                            let key = propArray[pa];
+                            if (pa == propArray.length - 1) {
+                                ref[key] = normalizedProps[r];
+                            } else {
+                                ref = ref[key] = ref[key] || {};
+                            }
+                        }
+                    }
+                }
+                return resource;
+            },
+
+            removeEmptyParams(params) {
+                for (let p in params) {
+                    if (!params[p] || params[p] == 'undefined') {
+                        delete params[p];
+                    }
+                }
+                return params;
+            },
+
+            executeRequest(request) {
+                let self = this;
+                request.execute(function (response) {
+
+                    if (response.error) self.hasErrors = true;
+
+                    if (response.nextPageToken) self.nextPageToken = response.nextPageToken;
+                    else self.nextPageToken = null;
+                    if (response.prevPageToken) self.prevPageToken = response.prevPageToken;
+                    else self.prevPageToken = null;
+
+                    self.items = response.items;
+                });
+            },
+
+            handleAuthClick() {
+                this.GoogleAuth.signIn();
+            },
+
+            limitDescText(desc) {
+                let text = desc.slice(0, 250);
+
+                if (desc.length > 250)
+                    text += '...';
+
+                return text;
+            },
+
+            pageToken(token) {
+                if (token.length === 0) return '';
+
+                return token;
+            }
+
+        },
+        mounted() {
+            this.injectGapi()
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
